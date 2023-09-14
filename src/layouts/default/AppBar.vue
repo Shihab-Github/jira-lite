7c76d1cd97e28f5d0005572f0dec90eaae152fe5
Raza <mahbub.razashihab@gmail.com>
2023-09-14 22:44:02 +0800

    setting up appbar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Jira Lite
-    </v-app-bar-title>
+  <v-app-bar>
+    <v-btn icon @click="">
+      <v-icon>mdi-menu</v-icon>
+    </v-btn>
+    <v-toolbar-title>Welcome, User</v-toolbar-title>
+    <v-spacer></v-spacer>
+    
+    <v-text-field placeholder="Search Tasks" prepend-inner-icon="mdi-magnify" variant="underlined"></v-text-field>
+    <v-btn icon @click="">
+      <v-icon>mdi-filter-check</v-icon>
+    </v-btn>
+    <v-btn icon @click="">
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+//
 </script>
