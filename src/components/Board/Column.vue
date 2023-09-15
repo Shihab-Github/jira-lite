c606784e373f7081c4e843a834a0958b9ee9f8fe
Raza <mahbub.razashihab@gmail.com>
2023-09-15 11:19:32 +0800

    added column component

---
+++ src/components/Board/Column.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-sheet rounded elevation="5" class="pa-3">
+        <div class="d-flex justify-space-between align-center">
+            <div class="text-subtitle-2">{{name}}</div>
+            <v-btn size="x-small" icon @click="">
+                <v-icon>mdi-dots-horizontal</v-icon>
+            </v-btn>
+        </div>
+        <div class="d-flex mt-3">
+            <v-btn block prepend-icon="mdi-plus">Add a new card</v-btn>
+        </div>
+    </v-sheet>
+</template>
+
+<script setup>
+const { name } = defineProps({
+    name: String
+})
+
+console.log('column name: ', name)
+</script>
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -2,42 +2,19 @@
     <v-sheet class="pa-2 page-height board-bg">
         <v-row>
             <v-col xs="12" md="4" lg="4">
-                <v-sheet rounded elevation="5" class="pa-3">
-                    <div class="d-flex justify-space-between align-center">
-                        <div class="text-subtitle-2">To Do</div>
-                        <v-btn size="x-small" icon @click="">
-                            <v-icon>mdi-dots-horizontal</v-icon>
-                        </v-btn>
-                    </div>
-                </v-sheet>
-
+                <Column name="To Do" />
             </v-col>
             <v-col xs="12" md="4" lg="4">
-                <v-sheet rounded elevation="5" class="pa-3">
-                    <div class="d-flex justify-space-between align-center">
-                        <div class="text-subtitle-2">Doing</div>
-                        <v-btn size="x-small" icon @click="">
-                            <v-icon>mdi-dots-horizontal</v-icon>
-                        </v-btn>
-                    </div>
-                </v-sheet>
+                <Column name="Doing" />
             </v-col>
             <v-col xs="12" md="4" lg="4">
-                <v-sheet rounded elevation="5" class="pa-3">
-                    <div class="d-flex justify-space-between align-center">
-                        <div class="text-subtitle-2">Done</div>
-                        <v-btn size="x-small" icon @click="">
-                            <v-icon>mdi-dots-horizontal</v-icon>
-                        </v-btn>
-                    </div>
-                </v-sheet>
+                <Column name="Done" />
             </v-col>
-
         </v-row>
     </v-sheet>
 </template>
 
 <script setup>
-console.log('Boards')
+import Column from '../components/Board/Column'
 </script>
 
